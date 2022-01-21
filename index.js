ab73da8c1d696e7482a11682e71cb319047334c9
Benjamin <bboksner@gmail.com>
2022-01-21 14:51:45 -0500

    commit tree 7045fce1245ce8a0e4ef3dd425ea2fe070e90677

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if(someValue > 42){
+      return someValue - 42;
+    }
+    else if(someValue<42){
+      return 42 - someValue ;
+      
+    }
+  }
+  function distanceFromHqInFeet(Value){
+   return distanceFromHqInBlocks(Value) * 264;
+  }
+  function distanceTravelledInFeet(start, end){
+    return Math.abs((start*264) - (end*264));
+  }
+ function calculatesFarePrice(start, destination){
+   let feet = distanceTravelledInFeet(start, destination)
+   if (feet<400){
+    return 0;
+   }
+   else if(feet>400 && feet <2000){
+     return (feet - 400) * 0.02;
+   }
+   else if(feet>2000 && feet <2500){
+     return 25;
+   }
+   else if(feet>2500){
+     return 'cannot travel that far';
+   }
+ 
+ }
+ 
